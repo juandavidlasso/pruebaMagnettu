2117c8184dcae690beb14603f6cd0bcde427d4c8
Juan David Lasso <juandavidlasso96@gmail.com>
2026-01-05 07:54:25 -0500

    ajuste para ejecutar en modo produccion

--- server-magnettu/src/shared/llm/llm.service.ts
+++ server-magnettu/src/shared/llm/llm.service.ts
@@ -1,9 +1,10 @@
-import { GoogleGenAI } from "@google/genai";
 import { LLMResponse } from "./llm.interface";
 import { LLMError } from "./llm.error";
 
-function createGeminiClient() {
+async function createGeminiClient() {
   const apiKey = process.env.GEMINI_API_KEY;
+  const mod = await import("@google/genai");
+  const { GoogleGenAI } = mod;
 
   if (!apiKey) {
     throw new Error("GEMINI_API_KEY missing");
@@ -12,13 +13,12 @@ function createGeminiClient() {
   return new GoogleGenAI({ apiKey });
 }
 
-const genAI = createGeminiClient();
-
 export async function generatePost(prompt: string): Promise<LLMResponse> {
   try {
     if (!prompt || prompt.trim() === "") {
       throw new LLMError("Prompt is empty");
     }
+    const genAI = await createGeminiClient();
 
     const res = await genAI.models.generateContent({
       model: "gemini-2.5-flash",
